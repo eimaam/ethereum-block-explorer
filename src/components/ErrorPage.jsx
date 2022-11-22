a1b6c9600686968c8b4b03053693714f8908cf2b
eimaam <imamddahir@gmail.com>
2022-11-22 15:20:30 +0100

    added error page for error navigation handling

---
+++ src/components/ErrorPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const ErrorPage = () => {
+  return (
+    <div className='container'>
+        <div className='container--child'>
+            
+
+        </div>
+    </div>
+  )
+}
