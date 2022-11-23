94edc46f5b5b45c3df7b0e95afb9a7d1285d1040
eimaam <imamddahir@gmail.com>
2022-11-23 07:32:50 +0100

    tablet styling added for responsiveness

--- src/components/TopNav.jsx
+++ src/components/TopNav.jsx
@@ -8,6 +8,7 @@ export const TopNav = () => {
     const show = () => {
         const input = document.querySelector("input")
         const searchBtn = document.querySelector("#searchBtn")
+        const cancelBtn = document.querySelector("#cancelBtn")
         const searchIcon = document.querySelector("#search")
         const title = document.querySelector("h3")
         const walletConnect = document.querySelector("#walletConnect")
@@ -15,13 +16,18 @@ export const TopNav = () => {
         if(input.style.display !== "block"){
             input.style.display = "block"
             searchBtn.style.display = "block"
+            cancelBtn.style.display = "block"
             searchIcon.style.display = "none"
             title.style.display = "none"
             walletConnect.style.display = "none"
         }else{
             input.style.display = "none"
+            searchBtn.style.display = "none"
+            cancelBtn.style.display = "none"
+            searchIcon.style.display = "block"
+            title.style.display = "block"
+            walletConnect.style.display = "block"
         }
-         
     }
 
   return (
@@ -34,8 +40,9 @@ export const TopNav = () => {
             <div className='flex-row'>
                 <div className='flex-row'>
                     <input type="text" placeholder='enter ETH address'/>
-                    <button type='submit' id='searchBtn'>SEARCH</button>
-                    <h2 id='search' onClick={show}><FaSearch /></h2>
+                    <button type='submit' id='searchBtn'>Search</button>
+                    <button type='submit' id='cancelBtn' onClick={show}>Cancel</button>
+                    <h2 id='searchIcon' onClick={show}><FaSearch /></h2>
                 </div>
                 <div id='walletConnect'>
                     <button className='flex-row' style={{gap: "0.2rem"}}>
