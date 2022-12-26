f9979cebbc5673122aee11bc4b9bcaf0fe2b7644
eimaam <imamddahir@gmail.com>
2022-12-26 12:37:27 +0100

    feat: added avatar to nav bar and account settings menu

--- src/components/homepage/Nav.jsx
+++ src/components/homepage/Nav.jsx
@@ -1,18 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import { MdLogout } from "react-icons/md"
+import { AiFillSetting } from "react-icons/ai"
+import { FaWallet } from "react-icons/fa"
+import avatar from "../../assets/IMG_0422 (2).HEIC"
 
 export const Nav = () => {
+  const [showSettingPanel, setShowSettingPanel] = useState(false)
   return (
-    <nav className='bg-primary p-2 border boder-b-grey'>
-      <ul className='w-60 flex--row m-auto justify-between sm:w-full'>
+    <nav className='bg-primary p-2 border boder-b-grey '>
+      <ul className='w-60 flex flex-row m-auto justify-between sm:w-full'>
         <div className='flex--row'>
-          <li className='text-lg sm:hidden'>Home</li>
-          <li className='text-lg sm:hidden'>About Us</li>
+          <li className='text-base md:text-lg cursor-pointer'>Home</li>
+          <li className='text-base md:text-lg cursor-pointer sm:hidden'>About Us</li>
         </div>
         <div className='flex--row'>
-          <li><button className='sm:text-xs btn-secondary'>Connect Wallet</button></li>
-          <li><button className='sm:text-xs btn-secondary'>Get Our Mobile App</button></li>
-          <li><Link to="/dashboard"><button className='sm:text-xs'>Dashboard</button></Link></li>
+        <div className='flex--row'>
+          <li><button className='sm:text-xs btn-secondary '>Connect Wallet</button></li>
+          {/* <li><button className='sm:text-xs btn-secondary'>Get Our Mobile App</button></li> */}
+          <li><Link to="/dashboard"><button className='btn-secondary sm:text-xs'>Dashboard</button></Link></li>
+        </div>
+        <div className='flex--col' onClick={() => setShowSettingPanel(prev => !prev)}>
+          <img src={avatar} alt="" className='w-8 h-8 md:w-10 md:h-10 rounded-[100%]'/>
+        {showSettingPanel &&
+          <div className='bg-secondary flex flex-col justify-start items-start absolute top-12 md:top-16 sm:right-0 p-2 rounded-md cursor-pointer'>
+            <h4 className=' hover:text-primary'>
+              <Link to="/wallets" className='flex--row'>
+                <FaWallet /> 
+                Wallets
+              </Link>
+            </h4>
+            <h4 className=' hover:text-primary'>
+              <Link to="/wallets" className='flex--row'>
+                <AiFillSetting /> 
+                Account Settings
+              </Link>
+            </h4>
+            <h4 className='flex--row hover:text-primary'>
+              <MdLogout /> 
+              Log Out
+            </h4>
+          </div>
+        }
+        </div>
         </div>
       </ul>
     </nav>
