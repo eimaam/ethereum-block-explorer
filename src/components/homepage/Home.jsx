6775252d0ee050b633e3acaedfdec9a4ce527a0a
eimaam <imamddahir@gmail.com>
2022-12-26 22:24:13 +0100

    fix: changed width of divs

--- src/components/homepage/Home.jsx
+++ src/components/homepage/Home.jsx
@@ -12,20 +12,20 @@ export const Home = () => {
     <>
     <Nav />
     <div className='w-full sm:min-h-70vh md:h-70vh lg:h-70vh flex--col bg-primary py-2'>
-        <div className='w-70 grid grid-cols-2 gap-6 md:w-80 sm:flex--col sm:flex-col-reverse'>
-            <div className='flex--col items-start' data-aos="fade-left">
+        <div className='grid grid-cols-2 gap-6 md:w-80 sm:flex--col sm:flex-col-reverse'>
+            <div className='w-[70%] flex--col items-start' data-aos="fade-left">
                 <h1 className='text-3xl font-bold sm:text-xl'>One environment to manage all your assets on the Ethereum Network</h1>
                 <h2 className='text-default text-base'>Securely Review your transactions and Manage all your ERC20 Tokens from an easy-to navigate Dashboard</h2>
                 <button className='py-4 px-6 bg-secondary text-md text-dark btn-secondary'>
                   <Link to="/dashboard">Get Started</Link></button>
             </div>
-            <div className='w-full'>
+            <div className='w-[70%] lg:w-full md:w-full'>
                 <img src={banner} alt="" data-aos="fade-right"/>
             </div>
         </div>
     </div>
     <div className='w-full my-14 flex--col' data-aos="fade-up">
-      <div className='w-70 grid grid-cols-2 gap-20 md:w-80 sm:flex sm:flex-col'>
+      <div className='w-[70%] grid grid-cols-2 gap-20 md:w-80 sm:flex sm:flex-col'>
         <div>
           <img src={tokens} alt="ethereum tokens" />
         </div>
