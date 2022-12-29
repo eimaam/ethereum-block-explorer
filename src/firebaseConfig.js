5e9e73866d7c89a93f1cdf8ebef07f85b73c4e26
eimaam <imamddahir@gmail.com>
2022-12-29 15:27:40 +0100

    fix: fixed firebase config location

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,12 +1,13 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+// firebase functions from SDK 
+import { getApp, initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getFirestore } from "firebase/firestore"
-import { getStorage } from "firebase/storage"
 import { GoogleAuthProvider, getAuth } from "firebase/auth"
+import { getFirestore } from "firebase/firestore"
 
+
+// configuration 
 const firebaseConfig = {
-    apiKey: `${process.env.REACT_APP_API_KEY}`,
+    apiKey: `${process.env.REACT_APP_FIREBASE_API_KEY}`,
     authDomain: `${process.env.REACT_APP_AUTH_DOMAIN}`,
     projectId: `${process.env.REACT_APP_PROJECT_ID}`,
     storageBucket: `${process.env.REACT_APP_STORAGE_BUCKET}`,
@@ -15,10 +16,14 @@ const firebaseConfig = {
     measurementId: `${process.env.REACT_APP_MEASUREMENT_ID}`
 };
 
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 export const auth = getAuth(app)
 export const database = getFirestore(app)
 export const googleProvider = new GoogleAuthProvider()
-export const storage = getStorage(app)
+
+
+
+
