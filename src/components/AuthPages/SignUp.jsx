2e29f5f60ecfa72cafe667201c4eb83c54d6c3a3
eimaam <imamddahir@gmail.com>
2022-12-29 16:14:55 +0100

    feat: added login command

--- src/components/AuthPages/SignUp.jsx
+++ src/components/AuthPages/SignUp.jsx
@@ -7,11 +7,17 @@ import { Link } from 'react-router-dom'
 // import { LoaderFullscreen } from './LoaderFullscreen'
 import { CgGoogle } from 'react-icons/cg'
 import { ResetPasswordModal } from './ResetPasswordModal'
+import { createUserWithEmailAndPassword } from 'firebase/auth'
+import { setUserId } from 'firebase/analytics'
+import { auth } from '../../firebaseConfig'
+import { useAuth } from '../../context/AuthContext'
+import { toast } from 'react-toastify'
+import { doc, setDoc } from 'firebase/firestore'
 
 
 
 export const SignUp = () => {
-    // const { loading, setLoading, navigate, setIsLogged, error, setError, logInWithGoogle } = useAuth()
+    const { setUser, usersRef, loading, setLoading, navigate, logInWithGoogle } = useAuth()
 
     // useEffect(() => {
     //     onAuthStateChanged(auth, data => {
@@ -37,10 +43,56 @@ export const SignUp = () => {
         }))
     }
 
+    const signUp = async (e) => {
+        e.preventDefault()
+        setLoading(true)
+        if(data.password.length < 6){
+            setLoading(false)
+            return toast.error('Password must be at least 6 characters long')
+            // check if username already exists
+        }else if(data.password !== data.confirmPassword){
+            return toast.error('Password do not match')
+        }
+
+        try{
+            await createUserWithEmailAndPassword(auth, data.email, data.password)
+            .then((res) => {
+                setUser({
+                    email: res.email
+                })
+                setDoc(doc(usersRef, data.email), {
+                    email: data.email
+                })
+                toast.success('Signed up successfully...')
+                navigate('dashboard')
+                return setLoading(false)
+            })
+        }
+        catch(err){
+            if(err.code === 'auth/wrong-password'){
+                toast.error('Wrong Password')
+                }else if(err.code === 'auth/too-many-requests'){
+                    toast.error('Too many trials! You will have to reset your password to access this site!')
+                }else if(err.code === 'auth/user-not-found'){
+                    toast.error('User not found')
+                }else if(err.code === 'auth/email-already-in-use'){
+                    toast.error('Email already in use!')
+                }else if(err.code === 'auth/user-not-found'){
+                    toast.error('User not found!')
+                }else if(err.code === 'auth/network-request-failed'){
+                    toast.error('Sorry...! Something went wrong. Check your internet connection')
+                }
+                else{
+                    setLoading(false)
+                    console.log(err.message)
+            }
+        }
+        
+    }
     
   return (
     <div className='w-full h-screen flex--col text-center' id='login'>
-        <form className='border rounded-md flex--col w-80 lg:w-1/3 md:w-1/2 p-4'>
+        <form onSubmit={signUp} className='border rounded-md flex--col w-90 lg:w-1/3 md:w-1/2 p-4'>
             <h2>Hi there! 👋 Welcome!</h2>
             <h3>Enter details to sign up:</h3>
             <div className='w-full'>
@@ -80,13 +132,12 @@ export const SignUp = () => {
                 or
             </p>
             <div>
-                <button className='btn-primary flex--row' style={{margin: "auto"}}>
+                <button type='button' className='btn-primary flex--row' style={{margin: "auto"}} onClick={logInWithGoogle}>
                     <CgGoogle /> Register with Google 
                 </button>
             </div>
             <div>
                 <p>Forgot Password? <button type='button' onClick={() => setShowModal(prev => !prev)} className="btn-small">RESET</button></p>
-                <p>or</p>
                 <p>Have an account already? &nbsp; 
                     <button className='btn-small'>
                         <Link to="/signup">Log in  </Link>
