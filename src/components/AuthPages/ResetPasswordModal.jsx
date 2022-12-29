e9f53ef078623ed7d19403de4f23bf7a46f06050
eimaam <imamddahir@gmail.com>
2022-12-29 16:19:52 +0100

    feat: fixed reset password modal

--- src/components/AuthPages/ResetPasswordModal.jsx
+++ src/components/AuthPages/ResetPasswordModal.jsx
@@ -1,7 +1,9 @@
 // import { sendPasswordResetEmail } from 'firebase/auth'
+import { sendPasswordResetEmail } from 'firebase/auth'
 import React, { useState } from 'react'
 import ReactDOM  from 'react-dom'
 import { toast } from 'react-toastify'
+import { auth } from '../../firebaseConfig'
 // import { useAuth } from '../Context/AuthContext'
 // import { auth } from '../firebaseConfig'
 
@@ -11,39 +13,36 @@ export const ResetPasswordModal = ({ handleClick }) => {
 
   const [email, setEmail] = useState("")
 
-  // function to send password reset mail
-//   const resetPassword = async (e) => {
-//     e.preventDefault()
-//     try{
-//       await sendPasswordResetEmail(auth, email)
-//       .then(() => {
-//         toast.info('Reset Email Sent! Check your Mailbox or Spam Folder')
-//         setMessage('Reset Email Sent! Check your Mailbox or Spam Folder')
-//       })
-//     }
-//     catch(err){
-//       if(err.code === "auth/user-not-found"){
-//         toast.error('User not found')
-//       }else{
-//         toast.error(err.code)
-//       }
-//     }
-//   }
+
+  const resetPass = async (e) => {
+    e.preventDefault()
+    try{
+      await sendPasswordResetEmail(auth, email)
+      .then(() => {
+        toast.info('Reset Email Sent! Check your Mailbox or Spam Folder')
+      })
+    }
+    catch(err){
+      if(err.code === "auth/user-not-found"){
+        toast.error('User not found')
+      }if(err.code === 'auth/missing-email'){
+        toast.error('Empty e-mail field...')
+      }else{
+        toast.error(err.code)
+      }
+    } 
+  }
 
   return ReactDOM.createPortal(
     <div className='fixed inset-0 bg-black/75 z-50'>
-        <form className='fixed top-1/2 left-1/2 translate-x-[-50%] flex--col px-10 py-12 w-1/2 rounded-md bg-primary'>
+        <form className='w-90 md:w-1/2 lg:w-1/2 fixed top-1/2 left-1/2 translate-x-[-50%] flex--col px-2 py-12 rounded-md bg-primary'>
               <input 
               type="email" 
               placeholder='Registered Email'
               onChange={(e) => setEmail(e.target.value)}
               required
               />
-              <input
-              className='btn-secondary w-1/2' 
-              type="submit" 
-              value="RESET"
-              />
+              <button type='submit' className='btn-secondary' onClick={resetPass}>RESET</button>
               <br />
               <button type='button' onClick={handleClick} className='btn-small'>close</button>
         </form>
