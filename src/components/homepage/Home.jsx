afaea54918c64963c78d2349f8d200d3d8a3790a
eimaam <imamddahir@gmail.com>
2023-01-03 21:25:57 +0100

    fix: resolved error in styling of header for desktop

--- src/components/homepage/Home.jsx
+++ src/components/homepage/Home.jsx
@@ -17,7 +17,7 @@ export const Home = () => {
                 <h1 className='text-3xl font-bold sm:text-xl'>One environment to manage all your assets on the Ethereum Network</h1>
                 <h2 className='text-default text-base'>Securely Review your transactions and Manage all your ERC20 Tokens from an easy-to navigate Dashboard</h2>
                 <button className='py-4 px-6 bg-secondary text-md text-dark btn-secondary'>
-                  <Link to="/dashboard">Get Started</Link></button>
+                  <Link to="/signup">Get Started</Link></button>
             </div>
             <div className='w-full lg:w-full md:w-full'>
                 <img src={banner} alt="" data-aos="fade-right"/>
