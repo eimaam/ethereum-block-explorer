4f630fe76ef2206012b27b113b9b24b342d283a8
eimaam <imamddahir@gmail.com>
2023-01-07 21:07:58 +0100

    styling: changed the margin, text and other content's sizes

--- src/components/homepage/Footer.jsx
+++ src/components/homepage/Footer.jsx
@@ -4,24 +4,24 @@ import ethLogo from "../../assets/EthereumLogo.svg"
 
 export const Footer = () => {
   return (
-    <footer className='w-full flex--col mt-48 border border-t-grey pt-20 sm:w-full'>
+    <footer className='w-full flex--col mt-48 border border-t-grey p-2 sm:w-full'>
         <div className='w-60 m-0 md:w-80'>
             <div className='flex--row justify-around sm:flex-col'>
                 <div className='flex--row sm:flex-col'>
-                    <img className='w-24 h-auto sm:w-10 sm:h-auto' src={ethLogo} alt="ethereum logo"/>
+                    <img className='w-14 h-auto sm:w-10 sm:h-auto' src={ethLogo} alt="ethereum logo"/>
                 </div>
                 <div className="flex--col">
-                    <h2 className='text-dark'>
+                    <h4 className='text-dark'>
                         Reach Us via:
-                    </h2>
+                    </h4>
                     <div className='flex--row gap-4'>
-                        <h1 className='text-primary hover:text-dark' ><FaLinkedinIn /></h1>
-                        <h1 className='text-primary hover:text-dark' ><FaTwitter /></h1>
-                        <h1 className='text-primary hover:text-dark' ><FaFacebook /></h1>
+                        <h2 className='text-primary hover:text-dark' ><FaLinkedinIn /></h2>
+                        <h2 className='text-primary hover:text-dark' ><FaTwitter /></h2>
+                        <h2 className='text-primary hover:text-dark' ><FaFacebook /></h2>
                     </div>
                 </div>
             </div>
-            <div className='flex--col gap-0 mt-6'>
+            <div className='flex--col gap-0 mt-2'>
                 <p>All rights reserved &copy; 2022</p>
             </div>
         </div>
