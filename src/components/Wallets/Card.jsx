6dafeeafe864d9f9215e3a87c104a94a515d581c
eimaam <imamddahir@gmail.com>
2023-01-17 20:29:31 +0100

    feat: added func to set wallet address and show wallet data

--- src/components/Wallets/Card.jsx
+++ src/components/Wallets/Card.jsx
@@ -1,18 +1,20 @@
 import React from 'react'
 import ethLogo from "../../assets/EthereumLogo2.png"
 import { MdDeleteForever } from "react-icons/md"
-import { Link } from 'react-router-dom'
+import { Link, Navigate } from 'react-router-dom'
+import { useData } from '../../context/DataContext'
+import { useAuth } from '../../context/AuthContext'
 
 
-export const Card = ({walletName, walletAddress, handleClick}) => {
+export const Card = ({walletName, walletAddress, handleDelete, handleClick}) => {
+  
   return (
-    <Link to="" className='m-auto flex flex-col justify-center gap-4 p-4'>
-      <div className='m-auto w-full bg-primary rounded-md p-4 flex flex-col gap-4'>
+      <div onClick={handleClick} className='m-auto w-full bg-primary rounded-md p-4 flex flex-col justify-center gap-4'>
         <div className='flex items-center justify-between'>
             <h2 className='text-dark text-lg md:text-2xl'>
                 {walletName}
             </h2>
-          <h2 className='text-dark text-lg md:text-2xl cursor-pointer' onClick={handleClick}><MdDeleteForever /></h2>
+          <h2 className='text-dark text-lg md:text-2xl cursor-pointer' onClick={handleDelete}><MdDeleteForever /></h2>
         </div>
         <div className='flex-row flex justify-start items-center'>
             <img src={ethLogo} alt="" className='w-14 w- md:w-20'/>
@@ -21,6 +23,5 @@ export const Card = ({walletName, walletAddress, handleClick}) => {
             </h2>
         </div>
       </div>
-    </Link>
   )
 }
--- src/components/Wallets/Wallets.jsx
+++ src/components/Wallets/Wallets.jsx
@@ -1,27 +1,33 @@
 import React from 'react'
-import { Footer } from '../homepage/Footer'
 import { Nav } from '../homepage/Nav'
 import { useState } from 'react'
 import { AddWalletModal } from '../../utilities/modals/AddWalletModal'
 import { useEffect } from 'react'
-import { collection, deleteDoc, doc, getDoc, getDocs, onSnapshot, query, where } from 'firebase/firestore'
+import { collection, deleteDoc, doc, onSnapshot, query, where } from 'firebase/firestore'
 import { database } from '../../firebaseConfig'
 import { Card } from './Card'
 import { toast } from 'react-toastify'
+import { useData } from '../../context/DataContext'
+import { useAuth } from '../../context/AuthContext'
 
 export const Wallets = () => {
+  const { walletAddress, setWalletAddress } = useData()
+  const { navigate } = useAuth()
+
+
+  const [walletList, setWalletList] = useState([])
   const [showModal, setShowModal] = useState(false)
-  const [userWallets, setUserWallets] = useState([])
+
 
   const [data, setData] = useState({
     walletName: "",
     walletAddress: "",
   })
   // checks if entered address is already registered else evaluates to undefined
-  const availableAddress = userWallets.find(item => item.walletAddress === data.walletAddress)
+  const availableAddress = walletList.find(item => item.walletAddress === data.walletAddress)
 
   // checks if entered wallet name is available already else evaluate to undefined
-  const availableWalletName = userWallets.find(item => item.walletName === data.walletName)
+  const availableWalletName = walletList.find(item => item.walletName === data.walletName)
 
   const handleChange = (e) => {
     const {name, value} = e.target
@@ -31,10 +37,29 @@ export const Wallets = () => {
     }))
   }
   
-  
+  useEffect(() => {
+    const fetchWalletList = async () => {
+      try {
+        const q = query(collection(database, 'wallets'), where('walletOwner', "==", "imamddahir@gmail.com"))
+        await onSnapshot(q, snapShot => {
+          setWalletList(snapShot.docs.map(data => ({
+            ...data.data(),
+            id: data.id
+          })))
+        })
+      } 
+      catch (error) {
+        console.log(error.message)
+      }
+    }
+        
+   fetchWalletList()
+
+  }, [])
+
 
   const deleteWallet = async (id, walletName) => {
-    const promptVal = prompt(`type \'delete\' to remove "${walletName}" Wallet`)
+    const promptVal = prompt(`type 'delete' to remove "${walletName}" Wallet`)
     if(promptVal === "delete"){
       try {
         await deleteDoc(doc(database, 'wallets', id))
@@ -50,38 +75,24 @@ export const Wallets = () => {
       return toast.error('incorrect entry... Retry!')
     }
   }
-  
 
-  useEffect(() => {
-    const fetchUserWallets = async () => {
-      try {
-        const q = query(collection(database, 'wallets'), where('walletOwner', "==", "imamddahir@gmail.com"))
-        await onSnapshot(q, snapShot => {
-          setUserWallets(snapShot.docs.map(data => ({
-            ...data.data(),
-            id: data.id
-          })))
-        })
-        
-      } catch (error) {
-        console.log(error.message)
-      }
-    }
-    fetchUserWallets()
-  }, [])
-
-
+  const setAddress = (address) => {
+    setWalletAddress(address)
+    navigate('/dashboard')
+  }
+  console.log(walletAddress)
   return (
     <>
     <Nav />
-    <div className='m-auto md:w-60 lg:w-1/2'>
-      <h1 className='rounded-md px-4 mt-4'>Linked Wallet(s): {userWallets.length}</h1>
-      {userWallets.map((item, index) => {
+    <div className='m-auto md:w-60 lg:w-1/2 p-1'>
+      <h1 className='rounded-md px-4 mt-4'>Linked Wallet(s): {walletList.length}</h1>
+      {walletList.map((item, index) => {
         return <Card 
                 key={index} 
                 walletName={item.walletName} 
                 walletAddress={item.walletAddress} 
-                handleClick={() => deleteWallet(item.id, item.walletName)}
+                handleDelete={() => deleteWallet(item.id, item.walletName)}
+                handleClick={() => setAddress(item.walletAddress)}
                 />
               }
       )}
